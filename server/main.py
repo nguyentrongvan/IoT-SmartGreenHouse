87923047d8ce520866d2d50ede6064587b3d2e3e
remtori <lqvu99@gmail.com>
2021-01-20 22:41:55 +0700

    Added graph drawing

--- server/main.py
+++ server/main.py
@@ -8,28 +8,37 @@ import threading
 import time
 import queue
 import json
+import random
+from serial_read_line import ReadLine
 from settings import SERIAL_PATH, WS_PORT, HTTP_PORT, URL_PATH, MAX_QUEUE_SIZE
 from web_server import run_web_server
 
-data_queue = queue.Queue(maxsize=MAX_QUEUE_SIZE)
-command = None
+mutex = threading.Lock()
+data_backlog = []
+active_data_queue = queue.Queue(maxsize=MAX_QUEUE_SIZE)
+command_queue = queue.Queue(maxsize=MAX_QUEUE_SIZE)
+clients = set()
 
 def serial_thread():
     global is_running
-    global command
+
     is_running = True
+    # while True:
+    #     time.sleep(1)
+    #     update_data('{}|{}'.format(random.randrange(25, 35), random.randrange(400, 600)).encode('utf-8'))
+
     with serial.Serial(SERIAL_PATH, 9600, timeout=1) as ser:
+        reader = ReadLine(ser)        
         while is_running:
-            line = ser.readline()
-            update_data(line.strip())
+            line = reader.readline(timeout=0.2)
+            if line is not None:
+                update_data(line.strip())
+
+            command = command_queue.get_nowait()
             if command is not None:
                 ser.write(str.encode(command))
-                command = None
 
-def update_data(s):
-    if data_queue.full():
-        data_queue.get()
-
+def update_data(s):        
     try:
         ss = s.decode('utf-8').split('|')
         if len(ss) != 2: 
@@ -40,30 +49,63 @@ def update_data(s):
     data = {
         "temp": int(ss[0]),
         "mois": int(ss[1]),
-        "time": int(time.time()), 
+        "time": int(time.time() * 1000), 
     }
-    data_queue.put_nowait(data)
+
+    if active_data_queue.full():
+        add_to_backlog(active_data_queue.get_nowait())
+
+    active_data_queue.put_nowait(data)
+
+def add_to_backlog(data):
+    mutex.acquire()
+    if len(data_backlog) == MAX_QUEUE_SIZE:
+        data_backlog.pop(0)
+        
+    data_backlog.append(data)
+    mutex.release()
 
 async def ws_server(websocket, path):
-    global command
-    while True:
-        try:
-            data = data_queue.get(timeout=0.1)
-            await websocket.send(json.dumps(data))
-        except:
-            pass
 
-        try:
-            client_data = await asyncio.wait_for(websocket.recv(), timeout=0.1)
-            command = client_data
-            if not command.endswith('\n'):
-                command += '\n'
-        except:
-            pass
+    mutex.acquire()
+    try:
+        await websocket.send(json.dumps(data_backlog))
+    except:
+        pass
+    mutex.release()
+
+    clients.add(websocket)
+    try:    
+        while True:        
+            if not active_data_queue.empty():
+                data = active_data_queue.get_nowait()
+                str_data = json.dumps(data)
+
+                await asyncio.wait([ ws.send(str_data) for ws in clients ])
+
+                add_to_backlog(data)
+                data = None
+
+            try:
+                client_data = await asyncio.wait_for(websocket.recv(), timeout=0.1)
+                command = client_data
+                if not command.endswith('\n'):
+                    command += '\n'
+
+                command_queue.put_nowait(command)
+            except (asyncio.TimeoutError, queue.Empty):
+                continue
+            except:
+                break
+    except:
+        pass
+    finally:
+        clients.remove(websocket)
 
 threading.Thread(target=serial_thread).start()
 threading.Thread(target=run_web_server).start()
 
 start_ws_server = websockets.serve(ws_server, URL_PATH, WS_PORT)
+asyncio.get_event_loop().create_task
 asyncio.get_event_loop().run_until_complete(start_ws_server)
 asyncio.get_event_loop().run_forever()
--- server/web_server.py
+++ server/web_server.py
@@ -1,16 +1,22 @@
-from bottle import route, run, template
+from bottle import get, run, template, static_file
 from settings import HTTP_PORT, URL_PATH, WS_PORT
 
 html_content = '404 Not Found'
 
-@route('/')
+@get('/')
 def index():
-    global html_content
-    return html_content
+    # global html_content    
+    # return html_content
+    with open('./client/index.html', 'r') as index_file:
+        return template(index_file.read(), WEBSOCKET_PATH="ws://{}:{}".format(URL_PATH, WS_PORT))
+
+@get('/<filename:path>')
+def js(filename):
+    return static_file(filename, root='./client')
 
 def run_web_server():    
     global html_content
-    with open('./index.html', 'r') as index_file:
+    with open('./client/index.html', 'r') as index_file:
         html_content = template(index_file.read(), WEBSOCKET_PATH="ws://{}:{}".format(URL_PATH, WS_PORT))
 
     run(host=URL_PATH, port=HTTP_PORT)
